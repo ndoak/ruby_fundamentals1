2c54694d422c89e8cbe487255758af16fa830ef5
ndoak <natalieannedoak@gmail.com>
2016-01-12 14:33:04 -0500

    exercise 3

---
+++ exercise3.rb
@@ -0,0 +1,5 @@
+#Exercise 3 - .chomp clears extra space from end of string and .strip clears extra space from both start and end of string
+puts "What is your name"
+# gets
+# name = gets.strip
+name = gets.chomp
