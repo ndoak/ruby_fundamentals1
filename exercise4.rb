46d301f1854f406aa07d89e317a397517310f3da
ndoak <natalieannedoak@gmail.com>
2016-01-12 15:00:07 -0500

    changed order of if/else and elsif statements

--- exercise4.rb
+++ exercise4.rb
@@ -1,11 +1,12 @@
 #Bitmaker's Fizzbuzz problem
 (1..100).each do |x|
-  if x % 3 == 0
-    puts "Bit"
-  elsif x % 5 == 0
-    puts "Maker"
-  elsif x % 3 == 0 && x % 5 == 0
+  if (x % 3 == 0 && x % 5 == 0
     puts "BitMaker"
+  elsif (x % 3 == 0)
+    puts "Bit"
+  elsif (x % 5 == 0)
+    puts "Maker"
+
   else
     puts x
   end
